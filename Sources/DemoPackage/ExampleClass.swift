d5cb96a384b67865a8dd70ecfd485dbb313b1a16
alpturk.bostan <alpturk.bostan@n11.com>
2019-12-13 14:18:30 +0300

    Initial Commit

---
+++ Sources/DemoPackage/ExampleClass.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleClass.swift
+//  PackageManagerDemo
+//
+//  Created by alpturk.bostan on 13.12.2019.
+//  Copyright © 2019 alpturk.bostan. All rights reserved.
+//
+
+import UIKit
+
+public class ExampleClass: NSObject
+{
+    public static func getTitle() -> String
+    {
+        return "Swift Package Manager"
+    }
+}
